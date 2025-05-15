f02dc767fea486a6c5c7214ab2a17295089c8564
Mikael Picolo <mikael.picolo@laposte.net>
2025-05-15 17:18:29 +0200

    add unit and functionnal test for the new function

--- tests/test_functional_server.py
+++ tests/test_functional_server.py
@@ -160,3 +160,22 @@ def test_booking_without_enough_points(client, mocker, fake_data):
 
     assert b"you don&#39;t have enough points" in response.data
 
+
+def test_booking_more_than_available_places(client, mocker, fake_data):
+    clubs, competitions = fake_data
+    club = clubs[0]
+    competition = competitions[1]
+
+    mocker.patch("server.clubs", clubs)
+    mocker.patch("server.competitions", competitions)
+
+    client.post('/login', data={'email': club['email']}, follow_redirects=True)
+
+    response = client.post('/purchasePlaces', data={
+        'competition': competition['name'],
+        'club': club['name'],
+        'places': '7'
+    }, follow_redirects=True)
+
+    assert b"there are not enough places available" in response.data
+
--- tests/test_unitaire_server.py
+++ tests/test_unitaire_server.py
@@ -1,5 +1,5 @@
 import pytest
-from server import has_enough_points
+from server import has_enough_points, has_enough_places_available
 
 
 def test_has_enough_points_true():
@@ -10,3 +10,19 @@ def test_has_enough_points_true():
 def test_has_enough_points_false():
     club = {'points': '2'}
     assert has_enough_points(club, 3) is False
+
+
+def test_has_enough_places_available():
+    competition = {
+        'numberOfPlaces': '10',
+    }
+    placesRequested = 5
+    assert has_enough_places_available(competition, placesRequested) is True
+
+
+def test_has_not_enough_places_available():
+    competition = {
+        'numberOfPlaces': '8',
+    }
+    placesRequested = 10
+    assert has_enough_places_available(competition, placesRequested) is False
